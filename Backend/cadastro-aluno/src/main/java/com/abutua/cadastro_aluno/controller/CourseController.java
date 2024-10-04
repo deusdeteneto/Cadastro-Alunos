969f030b87af7c22ca354d5991a221964abacc36
Torres-Neto <torresneto.tn@gmail.com>
2024-10-03 21:49:35 -0300

    endpoint de cursos

--- Backend/cadastro-aluno/src/main/java/com/abutua/cadastro_aluno/controller/CourseController.java
+++ Backend/cadastro-aluno/src/main/java/com/abutua/cadastro_aluno/controller/CourseController.java
@@ -1,5 +1,29 @@
 package com.abutua.cadastro_aluno.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.abutua.cadastro_aluno.model.Course;
+
+@RestController
+@RequestMapping("/courses")
 public class CourseController {
+
+    private List<Course> courses = new ArrayList<>();
     
+    // Cenário necessário para executar os testes
+    public CourseController() {
+        courses.add(new Course(1, "JavaScript"));
+        courses.add(new Course(2, "Java"));
+        courses.add(new Course(3, "Python"));
+    }
+
+    @GetMapping
+    public List<Course> getAllCourses() {
+        return courses;
+    }    
 }
