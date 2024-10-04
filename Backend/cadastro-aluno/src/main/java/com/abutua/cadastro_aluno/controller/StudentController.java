c5164b53c18878ff2232bf122ab650860d75a51e
Torres-Neto <torresneto.tn@gmail.com>
2024-10-03 21:45:56 -0300

    Criado endpoits de Estudantes

--- Backend/cadastro-aluno/src/main/java/com/abutua/cadastro_aluno/controller/StudentController.java
+++ Backend/cadastro-aluno/src/main/java/com/abutua/cadastro_aluno/controller/StudentController.java
@@ -3,10 +3,16 @@ package com.abutua.cadastro_aluno.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abutua.cadastro_aluno.model.Student;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 @RequestMapping("/students")
@@ -21,4 +27,22 @@ public class StudentController {
         students.add(new Student(3, "Carlos Souza", "carlos@gmail.com", "(11) 99999-8888", 3, 3));
     }
     
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return students;
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable int id) {
+        return students.stream()
+                .filter(student -> student.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+    
+    @PostMapping
+    public void addStudents(@RequestBody Student student) {     
+        students.add(student);
+    }
+
 }
